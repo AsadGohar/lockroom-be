6976fd5ed9251efe7804b005da5fb0b324e7a138
AcidProTech <asad1gohar@gmail.com>
2024-05-30 16:14:08 +0500

    updated rearrange api endpoint

--- src/folders/folders.controller.ts
+++ src/folders/folders.controller.ts
@@ -64,7 +64,7 @@ export class FoldersController {
   }
 
   @UseGuards(AuthGuard)
-  @Patch('rearragne')
+  @Patch('rearrange')
   rearragne(
     @Body('data') data: any,
     @Body('organization_id') organization_id: string,
